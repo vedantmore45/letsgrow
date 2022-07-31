5094d18738e629bc06d80655f80862b981056c69
vedantmore45 <88537437+vedantmore45@users.noreply.github.com>
2022-07-31 16:21:53 +0530

    Add files via upload

---
+++ TODO/app.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+    let task = document.querySelector(".input-box");
+    let list = document.querySelector(".line");
+    let addTask = document.querySelector(".btn");
+
+    addTask.addEventListener("click", () => {
+        list.insertAdjacentHTML(
+            `afterbegin`,
+            `<li>
+                    <i class= "fas fa-trash-alt deleteTask"></i>
+
+                    <h4>${task.value}</h4>
+            </li>`
+        );
+        task.value = "";
+
+        let deleteTask = document.querySelectorAll("deleteTask");
+
+        deleteTask.forEach((del) => {
+            del.addEventListener("click", () => {
+                del.parentElement.remove();
+            });
+        });
+    });
+};
